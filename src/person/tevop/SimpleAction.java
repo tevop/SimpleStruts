79f8f8f66a4fabdfe1c6212553876595bc9fb4db
tevop <tevop@163.com>
2019-02-03 11:10:34 +0800

    first commit

---
+++ src/person/tevop/SimpleAction.java
@@ -0,0 +1,19 @@
+package person.tevop;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class SimpleAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String execute() throws Exception {
+		return "success";
+	}
+	
+	
+
+}
